0ecc3feebcef21b3d3f0eb03a7a223295f28a88b
Patrick Imbrizi <patrick.imbrizi@gmail.com>
2024-02-21 16:43:11 -0300

    fix: db on prod

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const port = 3000;
 
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/*": "/$1"
+  })
+)
+
 server.use(router);
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+module.exports = server;
